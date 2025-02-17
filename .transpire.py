94bd76bd1f9cdb80c8536392adcfbe9580639271
Albert Ye <y.a.728@protonmail.ch>
2025-02-17 13:22:24 -0800

    some updates, not complete at all

--- .transpire.py
+++ .transpire.py
@@ -1,24 +1,42 @@
-from pathlib import Path 
-
+from pathlib import Path
 from transpire.resources import Deployment, Ingress, Secret, Service
+from transpire.utils import get_image_tag
 
 name = "rt"
 auto_sync = True 
 
 def objects():
-    yield Service(
-        name = "service"
+    svc = Service(
+        name = "rt-service",
+        selector = {
+            'app': name,
+        },
+        port_on_pod = 80,
+        port_on_svc = 80,
         # fill rest later..
     )
-    yield Deployment(
-        name = "deployment"
+    dep = Deployment(
+        name = "rt-deployment",
+        image = get_image_tag(name),
         # fill rest later..
     )
-    yield Ingress(
-        name = "virtual-host-ingress"
+    ing = Ingress.from_service(
+        svc = svc,
+        host = "//rt.ocf.berkeley.edu",
+        path_prefix = "/",
+    )
+    sec = Secret(
+        name = "keycloak-secret",
+        string_data = {
+            # not sure how to translate the yaml for this one...
+            'secret': 'placeholder',
+            'encryption_key': 'placeholder',
+        }
         # fill rest later..
     )
-    yield Secret(
-        name = "keycloak-secret"
-        # fill rest later..
-    )
+
+    # build all objects and yield.
+    yield svc.build()
+    yield dep.build()
+    yield ing.build()
+    yield sec.build()
