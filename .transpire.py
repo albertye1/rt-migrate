996995d9e4ef5b4e1894fe53515f35d66a3a16b8
Albert Ye <y.a.728@berkeley.edu>
2025-03-02 00:55:51 -0800

    translated deployment info

--- .transpire.py
+++ .transpire.py
@@ -1,13 +1,15 @@
-from pathlib import Path
+from tkinter.filedialog import Directory
 from transpire.resources import Deployment, Ingress, Secret, Service
 from transpire.utils import get_image_tag
 
+from kubernetes import client
+
 name = "rt"
-auto_sync = True 
+auto_sync = True
 
 def objects():
     svc = Service(
-        name = "rt-service",
+        name = "rt",
         selector = {
             'app': name,
         },
@@ -16,11 +18,12 @@ def objects():
         # fill rest later..
     )
     dep = Deployment(
-        name = "rt-deployment",
-        image = get_image_tag(name),
-        # fill rest later..
+        name = "rt",
+        image = get_image_tag("rt"),
+        ports = [ 80 ],
     )
-    ing = Ingress.from_service(
+
+    ing = Ingress.from_svc(
         svc = svc,
         host = "//rt.ocf.berkeley.edu",
         path_prefix = "/",
@@ -28,12 +31,45 @@ def objects():
     sec = Secret(
         name = "keycloak-secret",
         string_data = {
-            # not sure how to translate the yaml for this one...
-            'secret': 'placeholder',
-            'encryption_key': 'placeholder',
+            'secret': '',
+            'encryption_key': '',
         }
-        # fill rest later..
     )
+
+    # set the volumes in the Deployment
+    dep.obj.spec.template.spec.volumes = [
+        client.V1Volume(
+            name = "secrets",
+            host_path = { "path": "/opt/share/kubernetes/secret/rt", "type": "Directory" }
+        ),
+    ]
+    # also set the container used in deployment
+    dep.obj.spec.template.spec.containers[0].name = "rt-nginx"
+    dep.obj.spec.template.spec.containers[0].env = [
+        client.V1EnvVar(
+            name = "SERVER_NAME",
+            value = "rt.ocf.berkeley.edu",
+        ),
+        client.V1EnvVar(
+            name = "CLIENT_SECRET",
+            value_from = client.V1EnvVarSource(
+                secret_key_ref = client.V1SecretKeySelector(key = "secret", name = sec.name)
+            )
+        ),
+        client.V1EnvVar(
+            name = "ENCRYPTION_KEY",
+            value_from = client.V1EnvVarSource(
+                secret_key_ref = client.V1SecretKeySelector(key = "encryption_key", name = sec.name)
+            )
+        )
+    ]
+    dep.obj.spec.template.spec.containers[0].volume_mounts = [
+        { "name": "secrets", "mountPath": "/opt/share/secrets/rt" },
+    ]
+
+    # and finally set deployment's dns settings.
+    dep.obj.spec.template.spec.dns_policy = "ClusterFirst"
+    dep.obj.spec.template.spec.dns_config = {"searches": ["ocf.berkeley.edu"]}
 
     # build all objects and yield.
     yield svc.build()
